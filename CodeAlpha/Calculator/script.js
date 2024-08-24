313be050949b8b86760fe501d977dc3aaa7c3156
maniteji <120230586+maniteji@users.noreply.github.com>
2024-08-24 15:35:35 -0600

    Add files via upload

---
+++ CodeAlpha/Calculator/script.js
@@ -0,0 +1,59 @@
+let currentOperand = '';
+let previousOperand = '';
+let operation = undefined;
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    if (number === '0' && currentOperand === '0') return;
+    currentOperand += number;
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function compute() {
+    let result;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = result;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    const display = document.getElementById('display');
+    display.innerText = currentOperand;
+}
